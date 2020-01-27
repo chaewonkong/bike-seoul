2fbf9c6b643070dd193c526b3bb75cdaf1b6964c
Kong Chae Won <chaewonkong@gmail.com>
2020-01-27 20:01:29 +0900

    feat: add useBike hook

---
+++ src/redux/hooks/usePosition.ts
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../stores/bikeReducer";
+import { getPosition, getBike } from "../actions/bikeActions";
+import { useCallback } from "react";
+
+export default function useScore() {
+  const position = useSelector((state: RootState) => state.position);
+  const bikes = useSelector((state: RootState) => state.bikeList);
+  const dispatch = useDispatch();
+
+  const onGetPosition = useCallback(pos => dispatch(getPosition(pos)), [
+    dispatch
+  ]);
+
+  const onGetBikes = useCallback(bikes => dispatch(getBike(bikes)), [dispatch]);
+
+  return { position, onGetPosition, onGetBikes };
+}
